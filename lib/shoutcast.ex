5b18db7cf3b3ee4e5ef7744709b1156df624e1c4
Ryan Winchester <code@ryanwinchester.ca>
2018-01-13 15:16:28 -0800

    update a bit

--- lib/shoutcast.ex
+++ lib/shoutcast.ex
@@ -1,8 +1,14 @@
 defmodule Shoutcast do
 
   defmodule Meta do
-    defstruct [:offset, :length, :data, :raw]
-    @type t :: %__MODULE__{data: map, offset: integer, length: integer, raw: binary}
+    defstruct [:offset, :length, :data, :raw, :string]
+    @type t :: %__MODULE__{
+      data: map,
+      offset: integer,
+      length: integer,
+      raw: binary,
+      string: String.t
+    }
   end
 
   @doc """
@@ -18,17 +24,24 @@ defmodule Shoutcast do
   def read_meta(url) do
     {:ok, _status, headers, ref} = :hackney.get(url, [{'Icy-Metadata', '1'}], "", [])
 
-    offset =
-      headers
-      |> Enum.into(%{})
-      |> Map.get("icy-metaint")
-      |> String.to_integer()
+    offset = get_offset(headers)
 
     {:ok, data} = read_body(offset + 4081, ref, <<>>)
 
-    extract_meta(data, offset)
+    {meta_length, meta} = extract_meta(data, offset)
+
+    {:ok,
+      %Meta{
+        data: process_meta(meta),
+        offset: offset,
+        length: meta_length,
+        raw: meta,
+        string: String.trim(meta, <<0>>)
+      }
+    }
   end
 
+  # Stream the body until we get what we want.
   defp read_body(max_length, ref, acc) when max_length > byte_size(acc) do
     case :hackney.stream_body(ref) do
       {:ok, data}      -> read_body(max_length, ref, <<acc::binary, data::binary>>)
@@ -39,6 +52,15 @@ defmodule Shoutcast do
 
   defp read_body(_, _, acc), do: {:ok, acc}
 
+  # Get the byte offset from the `icy-metaint` header.
+  defp get_offset(headers) do
+    headers
+    |> Enum.into(%{})
+    |> Map.get("icy-metaint")
+    |> String.to_integer()
+  end
+
+  # Extract the meta data from the binary file stream.
   defp extract_meta(data, offset) do
     << _::binary-size(offset), length::binary-size(1), chunk::binary >> = data
 
@@ -49,9 +71,10 @@ defmodule Shoutcast do
 
     << meta::binary-size(meta_length), _::binary >> = chunk
 
-    {:ok, %Meta{offset: offset, length: meta_length, raw: meta, data: process_meta(meta)}}
+    {meta_length, meta}
   end
 
+  # Process the binary meta data into a map.
   defp process_meta(meta) do
     meta
     |> String.trim_trailing(<<0>>)
